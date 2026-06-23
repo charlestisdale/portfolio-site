111f26ecab3276a1277f0d0a3bb1ac7585ef9651
charlestisdale <charles.i.tisdale@gmail.com>
2026-06-23 16:40:06 -0400

    Export search facet helper

--- fundamentalstrainer/engine/knowledge/index.js
+++ fundamentalstrainer/engine/knowledge/index.js
@@ -1,4 +1,4 @@
 export { KnowledgeEngine } from "./knowledge-engine.js";
 export { JsonContentSource } from "./content-source.js";
 export { KnowledgeGraph } from "./graph.js";
-export { searchObjects } from "./search.js";
+export { searchObjects, collectSearchFacets } from "./search.js";
